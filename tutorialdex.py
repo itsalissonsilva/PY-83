03a9012d557a53e1f0c0e077dfa8ae70963821dc
thatguysilva <83712001+thatguysilva@users.noreply.github.com>
2021-05-22 16:29:35 -0300

    Update tutorialdex.py

--- tutorialdex.py
+++ tutorialdex.py
@@ -16,7 +16,7 @@ root = Tk()
 root.title("PY-83")
 
 e = Entry(root, width=35, borderwidth=5)
-e.grid(row =0, column=0,columnspan=3, padx=10, pady=10)
+e.grid(row =1, column=0,columnspan=3, padx=10, pady=10)
 
 #class Root(Tk):
 #    def _init_(self):
@@ -44,6 +44,7 @@ def button_click(number):
 
 def button_clear():
     e.delete(0,END)
+    f.clf()
 
 def button_add():
     first_number = e.get()
@@ -97,74 +98,91 @@ def button_divide():
 
 
 
-button_1 = Button(root, text="1", padx=40,pady=20,command=lambda: button_click(1))
-button_2 = Button(root, text="2", padx=40,pady=20,command=lambda: button_click(2))
-button_3 = Button(root, text="3", padx=40,pady=20,command=lambda: button_click(3))
-button_4 = Button(root, text="4", padx=40,pady=20,command=lambda: button_click(4))
-button_5 = Button(root, text="5", padx=40,pady=20,command=lambda: button_click(5))
-button_6 = Button(root, text="6", padx=40,pady=20,command=lambda: button_click(6))
-button_7 = Button(root, text="7", padx=40,pady=20,command=lambda: button_click(7))
-button_8 = Button(root, text="8", padx=40,pady=20,command=lambda: button_click(8))
-button_9 = Button(root, text="9", padx=40,pady=20,command=lambda: button_click(9))
-button_0 = Button(root, text="0", padx=40,pady=20,command=lambda:button_click(0))
-button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
-button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+button_1 = Button(root, text="1", padx=40,pady=10,command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40,pady=10,command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40,pady=10,command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40,pady=10,command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40,pady=10,command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40,pady=10,command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40,pady=10,command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40,pady=10,command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40,pady=10,command=lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40,pady=10,command=lambda:button_click(0))
+button_add = Button(root, text="+", padx=39, pady=10, command=button_add)
+button_equal = Button(root, text="=", padx=91, pady=10, command=button_equal)
+button_clear = Button(root, text="Clear", padx=79, pady=10, command=button_clear)
 
-button_subtract = Button(root, text="-", padx=40, pady=20, command=button_subtract)
-button_multiply = Button(root, text="×", padx=39, pady=20, command=button_multiply)
-button_divide = Button(root, text="÷", padx=39, pady=20, command=button_divide)
+button_subtract = Button(root, text="-", padx=40, pady=10, command=button_subtract)
+button_multiply = Button(root, text="×", padx=39, pady=10, command=button_multiply)
+button_divide = Button(root, text="÷", padx=39, pady=10, command=button_divide)
 
 
-button_1.grid(row=3,column=0)
-button_2.grid(row=3,column=1)
-button_3.grid(row=3,column=2)
+button_1.grid(row=4,column=0)
+button_2.grid(row=4,column=1)
+button_3.grid(row=4,column=2)
 
-button_4.grid(row=2,column=0)
-button_5.grid(row=2,column=1)
-button_6.grid(row=2,column=2)
+button_4.grid(row=3,column=0)
+button_5.grid(row=3,column=1)
+button_6.grid(row=3,column=2)
 
-button_7.grid(row=1,column=0)
-button_8.grid(row=1,column=1)
-button_9.grid(row=1,column=2)
+button_7.grid(row=2,column=0)
+button_8.grid(row=2,column=1)
+button_9.grid(row=2,column=2)
 
-button_0.grid(row=4,column=0)
-button_clear.grid(row=4, column=1, columnspan=2)
-button_add.grid(row=5,column=0)
-button_equal.grid(row=5, column=1, columnspan=2)
+button_0.grid(row=5,column=0)
+button_clear.grid(row=5, column=1, columnspan=2)
+button_add.grid(row=6,column=0)
+button_equal.grid(row=6, column=1, columnspan=2)
 
-button_subtract.grid(row=6, column=0)
-button_multiply.grid(row=6, column=1)
-button_divide.grid(row=6, column=2)
+button_subtract.grid(row=7, column=0)
+button_multiply.grid(row=7, column=1)
+button_divide.grid(row=7, column=2)
 
 
 #graphpartstartshere
 
-ent = Entry(root, width =50)
-ent.grid(row =1, column=0,columnspan=3, padx=10, pady=10)
+#ent = Entry(root, width =50)
+#ent.grid(row =1, column=0,columnspan=3, padx=10, pady=10)
 
 
 
-f = Figure(figsize=(5,5), dpi=100)
+f = Figure(figsize=(4,3), dpi=100)
 a = f.add_subplot(111)
+canvas = FigureCanvasTkAgg(f, root)
+canvas.draw()
+canvas.get_tk_widget().grid(row=0,column=1)
+    #canvas = FigureCanvasTkAgg(f, root)
+    #canvas.draw()
+    #canvas.get_tk_widget().grid(row=0)#couldmodify this whole part can be
+    #brought outside...
 
+#toolbar = NavigationToolbar2Tk(canvas, root)
+#toolbar.update()
+canvas._tkcanvas.grid(row=0)#couldmodify
+
+    
 def myClick():
            # y = e.get() i'd like something like this to work, instead of line below
-    y = eval(ent.get())
+    y = eval(e.get())
     a.plot(x,y,'r')
     canvas = FigureCanvasTkAgg(f, root)
     canvas.draw()
-    canvas.get_tk_widget().grid()
+    canvas.get_tk_widget().grid(row=0,column=1)
+    #canvas = FigureCanvasTkAgg(f, root)
+    #canvas.draw()
+    #canvas.get_tk_widget().grid(row=0)#couldmodify this whole part can be
+    #brought outside...
 
     toolbar = NavigationToolbar2Tk(canvas, root)
     toolbar.update()
-    canvas._tkcanvas.grid()
+    canvas._tkcanvas.grid(row=0)#couldmodify
                     
                
 
 button4 = Button(root, text="plot", command = myClick)
-button4.grid(row=7,column=2)
+button4.grid(row=1,column=2)
 
 
 
 root.mainloop()
+
